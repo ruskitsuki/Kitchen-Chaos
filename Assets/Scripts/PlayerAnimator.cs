f45ea63d09555c4af418ec376c5e5a9d5b32eb51
Rusnanee Madeng <rusnanee.m07@gmail.com>
2026-01-17 16:30:29 +0700

    add project

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,20 @@
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+
+    [SerializeField] private Player player;
+    private Animator animator;
+
+    private void Awake()
+    {
+
+        animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("IsWalking",player.IsWalking());
+    }
+}
